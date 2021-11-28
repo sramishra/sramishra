eb948ee717f19efcb1a53a68dc0f0d7e797e691f
Sradha Mishra <sradha0707@gmail.com>
2021-11-28 19:28:01 +0530

    commit 8

--- src/com/promtionEngine/test/EngineTest.java
+++ src/com/promtionEngine/test/EngineTest.java
@@ -13,9 +13,9 @@ class EngineTest {
 	@Test
 	public void testScenarioA() {
 		HashMap<String, Integer> m = new HashMap();
-		m.put("A", 0);
-		m.put("B", 0);
-		m.put("C", 0);
+		m.put("A", 1);
+		m.put("B", 1);
+		m.put("C", 1);
 		m.put("D", 0);
 		HashMap<String, Integer> unitprice = new HashMap();
 		unitprice.put("A", 50);
@@ -26,5 +26,22 @@ class EngineTest {
 		int expected = 100;
 		assertEquals(expected,actual);
 	}
+	@Test
+	public void testScenarioB() {
+		HashMap<String, Integer> m = new HashMap();
+		m.put("A", 5);
+		m.put("B", 5);
+		m.put("C", 1);
+		m.put("D", 0);
+		HashMap<String, Integer> unitprice = new HashMap();
+		unitprice.put("A", 50);
+		unitprice.put("B", 30);
+		unitprice.put("C", 20);
+		unitprice.put("D", 15);
+		int actual = Engine.totalOrderValue(m, unitprice);
+		int expected = 370;
+		assertEquals(expected,actual);
+	}
+
 
 }
