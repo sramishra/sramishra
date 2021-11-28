1124f116f648f1c7d6707c226021427018146130
Sradha Mishra <sradha0707@gmail.com>
2021-11-28 19:31:52 +0530

    commit 12

--- src/com/promtionEngine/test/EngineTest.java
+++ src/com/promtionEngine/test/EngineTest.java
@@ -42,6 +42,21 @@ class EngineTest {
 		int expected = 370;
 		assertEquals(expected,actual);
 	}
-
+	@Test
+	public void testScenarioC() {
+		HashMap<String, Integer> m = new HashMap();
+		m.put("A", 3);
+		m.put("B", 5);
+		m.put("C", 1);
+		m.put("D", 1);
+		HashMap<String, Integer> unitprice = new HashMap();
+		unitprice.put("A", 50);
+		unitprice.put("B", 30);
+		unitprice.put("C", 20);
+		unitprice.put("D", 15);
+		int actual = Engine.totalOrderValue(m, unitprice);
+		int expected = 280;
+		assertEquals(expected,actual);
+	}
 
 }
